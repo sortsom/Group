6fe83594bf000fd15349214bd5d4541eaa6c9966
sortsom <som.ssp2018@gmail.com>
2021-08-07 16:35:25 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,7 +30,7 @@
             @foreach($users as $user)
                 <tr>
                     <td>{{ $user->id}}</td>
-                    <td>{{$user->image ?? ''}}</td>
+                    <td>{{$user->image}}</td>
                     <td>{{ $user->name}}</td>
                     <td>{{$user->email}}</td>
                     <td>{{$user->role}}</td>
@@ -42,9 +42,9 @@
                         <a href="#" data-toggle="modal" data-target="#delete-user-{{ $user->id }}">
                             <i class="align-middle" data-feather="trash"></i>
                         </a>
-
-                        <!--    editmodel-->
-                        <div class="modal fade" id="edit-user-{{$user->id}}" aria-hidden="true">
+                        <!--edited-->
+                        <div class="modal fade" id="edit-user-{{$user->id}}" tabindex="-1"
+                             aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
@@ -53,51 +53,43 @@
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
-                                    <form action="{{route('users.update',$user->id)}}" method="POST">
-                                        <div class="modal-body">
-
+                                        <form method="POST" action="{{'users.update',$user->id}}">
                                             @csrf
-                                            @method('PUT')
                                             <div class="form-group row">
-                                                <label for="name"
-                                                       class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
                                                 <div class="col-md-6">
-                                                    <input id="name" type="text"
-                                                           class="form-control @error('name') is-invalid @enderror"
-                                                           name="name" value="{{ $user->name }}" required
-                                                           autocomplete="name" autofocus>
+                                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                                           name="name" value="{{ $user->name}}" required autocomplete="name" autofocus>
                                                     @error('name')
                                                     <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
+                                                 <strong>{{ $message }}</strong>
                                                     @enderror
                                                 </div>
                                             </div>
                                             <div class="form-group row">
                                                 <label for="email"
-                                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail ') }}</label>
 
                                                 <div class="col-md-6">
-                                                    <input id="email" type="email"
-                                                           class="form-control @error('email') is-invalid @enderror"
-                                                           name="email" value="{{ $user->email }}" required>
-
+                                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                                           name="email" value="{{ $user->email}}" required autocomplete="email">
                                                     @error('email')
                                                     <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
+                                                     <strong>{{ $message }}</strong>
                                                     </span>
                                                     @enderror
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button class="btn btn-danger" data-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-success">Save delete</button>
-                                        </div>
-                                    </form>
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                               <button type="submit" class="btn btn-success">Save update</button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                                                {{--delete user--}}
 
                         <div class="modal fade" id="delete-user-{{ $user->id }}" tabindex="-1"
                              aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -132,4 +124,94 @@
         </table>
     </div>
 
-@endsection
+
+
+    <!-- Modal -->
+
+{{--    <!--    editmodel-->--}}
+{{--    <div class="modal fade" id="editModalCenter" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">--}}
+{{--        <div class="modal-dialog">--}}
+{{--            <div class="modal-content">--}}
+{{--                <div class="modal-header">--}}
+{{--                    <h5 class="modal-title" id="exampleModalLabel"><b>EDIT USER</b></h5>--}}
+{{--                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                        <span aria-hidden="true">&times;</span>--}}
+{{--                    </button>--}}
+{{--                </div>--}}
+{{--                <div class="modal-body">--}}
+{{--                    <form method="POST" action="#">--}}
+{{--                        @csrf--}}
+{{--                        <div class="form-group row">--}}
+{{--                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>--}}
+
+{{--                            <div class="col-md-6">--}}
+{{--                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"--}}
+{{--                                       name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>--}}
+{{--                                @error('name')--}}
+{{--                                <span class="invalid-feedback" role="alert">--}}
+{{--                                        <strong>{{ $message }}</strong>--}}
+{{--                                    </span>--}}
+{{--                                @enderror--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="form-group row">--}}
+{{--                            <label for="email"--}}
+{{--                                   class="col-md-4 col-form-label text-md-right">{{ __('E-Mail ') }}</label>--}}
+
+{{--                            <div class="col-md-6">--}}
+{{--                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"--}}
+{{--                                       name="email" value="{{ old('email') }}" required autocomplete="email">--}}
+
+{{--                                @error('email')--}}
+{{--                                <span class="invalid-feedback" role="alert">--}}
+{{--                                        <strong>{{ $message }}</strong>--}}
+{{--                                    </span>--}}
+{{--                                @enderror--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="form-group row">--}}
+{{--                            <label for="password"--}}
+{{--                                   class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
+{{--                            <div class="col-md-6">--}}
+{{--                                <input id="password" type="password"--}}
+{{--                                       class="form-control @error('password') is-invalid @enderror" name="password"--}}
+{{--                                       required autocomplete="new-password">--}}
+{{--                                <div class="form-group row">--}}
+{{--                                    <label for="password-confirm"--}}
+{{--                                           class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>--}}
+{{--                                    <div class="col-md-6">--}}
+{{--                                        <select name="role_od" id="role_id" class="form-control">--}}
+{{--                                            <option value="">--select--</option>--}}
+{{--                                            <option value="admin">administrator</option>--}}
+{{--                                            <option value="user">Teachers</option>--}}
+{{--                                            <option value="student">Students</option>--}}
+{{--                                        </select>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <div class="form-group row">--}}
+{{--                                    <label for="password-confirm"--}}
+{{--                                           class="col-md-4 col-form-label text-md-right">{{ __('images') }}</label>--}}
+{{--                                    <div class="col-md-6">--}}
+{{--                                        <div class="variants">--}}
+{{--                                            <div class='file'>--}}
+{{--                                                <label for='input-file'>--}}
+{{--                                                    <i class="fa fa-cloud" aria-hidden="true"></i>--}}
+{{--                                                    Select a file--}}
+{{--                                                </label>--}}
+{{--                                                <input id='input-file' type='file'/>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+
+{{--                        </div>--}}
+{{--                        <div class="modal-footer">--}}
+{{--                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>--}}
+{{--                    <button type="button" class="btn btn-success">Save update</button>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-Route::get('/administrator',[\App\Http\Controllers\HomeController::class,"index"]);
-Route::delete('/delete/{user}',[\App\Http\Controllers\HomeController::class,"destroy"])->name('users.delete');
-Route::put('/update/{user}',[\App\Http\Controllers\HomeController::class,"update"])->name('users.update');
+Route::get('/administrator',[HomeController::class,"index"]);
+Route::delete('/delete/{user}',[HomeController::class,"destroy"])->name('users.delete');
+Route::put('/update/{user}',[HomeController::class,"update"])->name('users.update');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
