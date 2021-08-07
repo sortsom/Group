e80ed032a64c443aebd019e9c4785d613de1722f
sortsom <som.ssp2018@gmail.com>
2021-08-07 15:12:50 +0700

    s

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,14 +47,6 @@ class HomeController extends Controller
         $users->delete();
         return redirect('/administrator')->with('Success', 'User is deleted');
     }
-    public function  edit($id)
-    {
-    }
-    public function update( Request $request, $id){
-        $user=User::findOrFail($id);
-        $user->update([
-            'name'=>$request->name,
-            'email'=>$request->email,
-        ]);
-    }
+
+
 }
